6c09836b0cdef7cd9a8ed15bef8d661a3a235483
webkubor <webkubor@163.com>
2026-02-28 14:44:27 +0800

    auto: 基础架构 at 2026/2/28 14:44:27

--- scripts/core/sentinel.js
+++ scripts/core/sentinel.js
@@ -8,6 +8,7 @@
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { execSync } from 'child_process';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
